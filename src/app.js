734c4e6eb97990c220d2cae049caa8d0c55adc0d
Nelson Cadena <32472985+ncadenaroman86@users.noreply.github.com>
2025-04-26 11:25:15 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+// Arrays for each part of the excuse
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed on', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car', 'my project'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+// Function to generate a random excuse
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
 
+// When page loads, set a new excuse
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
